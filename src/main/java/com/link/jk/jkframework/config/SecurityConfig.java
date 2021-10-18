9516f5b68d4ef76d8b33651688d1fa79df93c1c9
DEV-BACK04 <snjune6@naver.com>
2021-10-18 16:26:04 +0900

    시큐리티 시작

---
+++ src/main/java/com/link/jk/jkframework/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.link.jk.jkframework.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+}
