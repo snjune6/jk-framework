c4cb9e6d0653f0363066242482d1073ac1748f10
김준태 <snjune6@idino.co.kr>
2022-03-18 17:42:14 +0900

    -

--- src/main/java/com/link/jk/jkframework/JkFrameworkApplication.java
+++ src/main/java/com/link/jk/jkframework/JkFrameworkApplication.java
@@ -3,12 +3,19 @@ package com.link.jk.jkframework;
 import com.link.jk.jkframework.config.SiteMeshConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
-public class JkFrameworkApplication {
+public class JkFrameworkApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JkFrameworkApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JkFrameworkApplication.class, args);
